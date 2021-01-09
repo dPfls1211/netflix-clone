52a55717e87d86903f5ebbae2f7a58a952746b4d
dPfls1211 <76572665+dPfls1211@users.noreply.github.com>
2021-01-10 02:08:11 +0900

    pin-title-bar
    스크롤시 클래스 추가 해서 할려했는데 왜안될까ㅏㅏㅠㅠ 후에 꼭.. 하기

---
+++ title.js
@@ -0,0 +1,23 @@
+// const pinTitle = document.getElementById("pin-header-bar_fixed");
+
+// function _Handlescroll(){
+//     // $(window).scroll(function(){
+//     //     if($(this).scrollTop()>0){
+//     //        pinTitle.addClass('animateIn');
+//     //     }else{
+//     //        pinTitle.removeClass('animateIn');
+//     //     }
+//     // });
+//     document.addEventListener('scroll',function{
+//         if(document.documentElement.scrollTop>0){
+//             pinTitle.classList.add("animateIn");
+//         }
+//     });
+// }
+
+// function init(){
+//     _Handlescroll();
+//     pinTitle.addEventListener("scroll",_Handlescroll);
+// }
+// init();
+////안해.. ㅠ
